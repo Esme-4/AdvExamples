d41badf143dcd52e6eaacfacb02e4b92eb08c822
Fernando Barrios <fbarrios@unam.mx>
2021-12-19 12:53:40 -0600

    Update GLMexamp.R
    ANCOVA example with HERS

--- GLMexamp/GLMexamp.R
+++ GLMexamp/GLMexamp.R
@@ -12,15 +12,46 @@ HERS %>% get_summary_stats("weight", "BMI", "waist", "WHR", "glucose",
 
 # HERS data for non-diabetic
 HERS_nodi <- filter(HERS, diabetes == "no")
-HERS_nodi_lm <- lm(glucose ~ exercise, data = HERS_nodi)
-# Linear model results can be printed with summary
-summary(HERS_nodi_lm)
 
+ggplot(HERS_nodi, aes(x = BMI, y = LDL)) +
+  geom_point(shape = 16, aes(color = exercise)) +
+  labs(title = "Measurement of LDL vs. Body Mass Index") +
+  theme_bw()
+
+ggplot(HERS_nodi, aes(x = exercise, y = LDL)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = exercise), shape = 16, height = 0.1, width = 0.1) +
+  labs(title = "Measurement of LDL vs. exercise") +
+  theme_bw()
+
+HERSnodi_lm01 <- lm(LDL ~ BMI * exercise, data = HERS_nodi)
+summary.aov(HERSnodi_lm01)
+
+HERSnodi_lm02 <- lm(LDL ~ exercise * BMI, data = HERS_nodi)
+summary.aov(HERSnodi_lm02)
+
+# Estimate the simplest model, the interaction is not significant
+# fit different intercepts for exercise ne and yes with the same slope to both graphs
+HERSnodi_lm <- lm(LDL ~ exercise + BMI, data = HERS_nodi)
+anova(HERSnodi_lm02, HERSnodi_lm)
+
+summary.lm(HERSnodi_lm)
+
+ggplot(HERS_nodi, aes(x = BMI, y = LDL, color = exercise)) +
+  geom_point(shape = 20) +
+  geom_abline(aes(intercept = 133.05, slope = 0.5181)) +
+  geom_abline(aes(intercept = 133.05-4.16, slope = 0.5181)) +
+  labs(title = "ANCOVA of LDL vs. Body Mass Index with exercise") +
+  theme_bw()
+
+# glucose model
 ggplot(HERS_nodi, aes(x = exercise, y = glucose)) +
-         geom_boxplot() +
-         geom_jitter(aes(color = raceth), shape = 16, position = position_jitter(seed = 123)) +
-         labs(title = "Measurement of glucose") +
-         theme_bw()
+  geom_boxplot() +
+  geom_jitter(aes(color = exercise), shape = 20, height = 0.1, width = 0.1) +
+  labs(title = "Measurement of glucose") +
+  theme_bw()
 
-anova(HERS_nodi_lm)
+HERSnodi_glu <- lm(glucose ~ exercise, data = HERS_nodi)
+# Linear model results can be printed with summary
+summary(HERSnodi_glu)
 
